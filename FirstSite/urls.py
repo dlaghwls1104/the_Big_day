98d5254fe67fcc7c63ff68fdf56e8633e035e679
HOJIN <dlaghwls1104@naver.com>
2022-03-15 10:26:17 +0900

    final.commit

--- FirstSite/urls.py
+++ FirstSite/urls.py
@@ -6,11 +6,10 @@ urlpatterns = [
     path('qnaboard/', include('qnaboard.urls')),
     path('users/', include('users.urls')),
     path('challenges/', include('challenges.urls')),
-    path('bigday/', include('bigday.urls')),
+    path('', include('bigday.urls')),
     path('study/', include('study.urls')),
     path('map/', include('map.urls')),
     path('competition/', include('competition.urls')),
     path('group/', include('group.urls')),
     path('ranking/', include('ranking.urls')),
-
 ]

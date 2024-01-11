1f9f553e4bdb2949585e642748185cdfc4a58183
dlaghwls1104 <dlaghwls1104@naver.com>
2024-01-11 19:38:32 +0900

    datalayer

--- FirstSite/settings.py
+++ FirstSite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SsECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-hfssi)md)zjxl))qt)ejgi@mw^@g^iaz+(s!-y16^=q$vaj8x*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
